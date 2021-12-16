b5fc6965d2613811e621a5b15b884f5f4c60be0f
Americo Meneses <beatamez@hotmail.com>
2021-12-16 16:43:58 -0600

    first commit

---
+++ CoreDataBasics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataBasics
+//
+//  Created by Américo MQ on 16/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CoreDataBasics/CoreDataBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataBasicsApp.swift
+//  CoreDataBasics
+//
+//  Created by Américo MQ on 16/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
